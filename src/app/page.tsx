a4bd1c1f51b53ccbe29fbb11643dc831339dfd8e
FabsCR <fabfernandez@estudiantec.cr>
2024-11-14 03:35:37 -0600

    feat: work in progress card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,16 @@ export default function Home() {
       <header className="flex justify-end p-4">
         <ThemeToggle />
       </header>
+
+      <div className="flex justify-center items-center p-4">
+        <div className="bg-white text-gray-800 dark:bg-gray-800 dark:text-white rounded-lg shadow-lg w-full max-w-4xl p-4 animate-zoom mx-4">
+          <h1 className="text-2xl font-extrabold mb-2 text-center tracking-wider text-blue-600 dark:text-blue-300">Work in Progress 🚀</h1>
+          <p className="text-lg text-center font-semibold text-gray-700 dark:text-gray-300">
+            We&apos;re cooking up a <span className="italic">&quot;McLovin-style Card Generator&quot;</span> so you can make your own cards.
+          </p>
+        </div>
+      </div>
+
       <McLovin />
       <Footer />
     </main>
