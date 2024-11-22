6e391ff72916f329b3b481866d342a3e3e0a6fa2
FabsCR <fabianfdez06@gmail.com>
2024-11-22 02:07:27 -0600

    style: first implementation of animations for all the webpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import McLovinCard from "../components/McLovinCard";
 import Footer from "../components/Footer";
 import ThemeToggle from "../components/ThemeToggle";
+import McLovin from "../components/McLovin";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col justify-between bg-gray-100 dark:bg-gray-900">
-      <header className="flex flex-col items-center p-4">
+    <main className="min-h-screen flex flex-col justify-between bg-gray-100 dark:bg-gray-900 animate-fade-in">
+      <header className="flex flex-col items-center p-4 animate-slide-up">
         <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-100 text-center mb-4">
           McLovin Card Generator
         </h1>
         <ThemeToggle />
       </header>
-      <div className="flex justify-center items-center p-4">
+      <div className="flex justify-center items-center p-4 animate-slide-up">
         <McLovinCard />
+      </div>
+      <div className="animate-slide-up">
+        <McLovin />
       </div>
       <Footer />
     </main>
